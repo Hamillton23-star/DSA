e0a198d614920b67da3b5f9805e59f1a9b807b54
Hamillton23-star <Bhavygoyal011@gmail.com>
2025-06-29 14:29:49 +0530

    Time: 11 ms (29.35%) | Memory: 33 MB (93.92%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,27 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+class KthLargest {
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+        minHeap.push(val);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
