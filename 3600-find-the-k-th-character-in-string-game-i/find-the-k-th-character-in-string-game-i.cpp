8d37ae4bbbbbb1b07697570bba349434581900a8
Hamillton23-star <Bhavygoyal011@gmail.com>
2025-07-03 17:10:30 +0530

    Time: 4 ms (36.05%) | Memory: 10.2 MB (35.02%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+
+        while (word.length() < k) {
+            string next = "";
+            for (char c : word) {
+                next += (c == 'z') ? 'a' : c + 1;
+            }
+            word += next;
+        }
+
+        return word[k - 1]; 
+    }
+};
