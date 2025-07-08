615a689a1222ea4abdd01a9f5fb09722021df147
Hamillton23-star <Bhavygoyal011@gmail.com>
2025-07-08 18:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (29.36%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    TreeNode* findMin(TreeNode* node) {
+        while (node->left) node = node->left;
+        return node;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        if (key < root->val)
+            root->left = deleteNode(root->left, key);
+        else if (key > root->val)
+            root->right = deleteNode(root->right, key);
+        else {
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            TreeNode* succ = findMin(root->right);
+            root->val = succ->val;
+            root->right = deleteNode(root->right, succ->val);
+        }
+        return root;
+    }
+};
